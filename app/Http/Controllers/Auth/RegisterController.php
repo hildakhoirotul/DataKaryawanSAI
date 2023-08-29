706a7f88907d6259062e056b925238581e5fc716
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-08-29 15:25:09 +0700

    revisi register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -104,7 +104,7 @@ class RegisterController extends Controller
 
         $details = [
             'nik' => $request->nik,
-            'website' => 'http://192.168.94.229/',
+            'website' => env('APP_URL'),
             'datetime' => date('Y-m-d H:i:s'),
             'url' => request()->getHttpHost() . '/register/verify/' . $str
         ];
