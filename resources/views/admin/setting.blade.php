2cb62457cba227f5a2ff895bac2a7cb72df4c87e
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-08-29 13:11:08 +0700

    setting page

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -11,9 +11,19 @@
         <div class="card mt-2 px-4 py-3">
             <form id="switchForm" action="{{ route('disable.login') }}" method="post">
                 @csrf
-                <div class="form-check form-switch p-0 px-1 d-flex align-items-center justify-content-between">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Disable Detail OCHI & QCC</label>
-                    <input class="form-check-input custom-switch" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="status" {{ $status ? 'checked' : '' }}>
+                <div class="form-check form-switch p-0 ps-1 pe-3 d-flex align-items-center justify-content-between">
+                    <label class="form-check-label" style="display: flex;align-items: center;" for="flexSwitchCheckDefault">
+                        <i class='bx bx-cog me-2' style="font-size: 2rem;"></i>
+                        <span>Disable Detail OCHI & QCC</span>
+                    </label>
+                    <div style="display: flex; align-items: center;">
+                        @if($status)
+                        <span style="margin-right: 3.5rem;">On</span>
+                        @else
+                        <span style="margin-right: 3.5rem;">Off</span>
+                        @endif
+                        <input class="form-check-input custom-switch" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="status" {{ $status ? 'checked' : '' }}>
+                    </div>
                 </div>
             </form>
         </div>
