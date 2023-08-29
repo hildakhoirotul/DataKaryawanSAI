80e02ac051555e790a8302e37e8c8aaa4f169468
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-08-29 08:30:17 +0700

    revisi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,8 @@ use App\Models\Ochi;
 use App\Models\Qcc;
 use App\Models\Setting;
 use App\Models\Information;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {
@@ -21,33 +23,67 @@ class UserController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $a = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'A')
-            ->get();
-        $s = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'S')
-            ->get();
-        $sd = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'SD')
-            ->get();
-        $iz = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'I')
-            ->get();
-        $itd = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'ITD')
-            ->get();
-        $icp = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'ICP')
-            ->get();
-        $td = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'TD')
-            ->get();
-        $cp = Absensi::where('nik', $user->nik)
-            ->where('jenis', 'CP')
-            ->get();
-        $ochi = Ochi::where('nik', $user->nik)->get();
-        $qcc = Qcc::where('nik', $user->nik)->get();
-        $oleader = Ochi::where('nik_ochi_leader', $user->nik)->get();
+        $a = Cache::remember('absensi_a_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'A')->get();
+        });
+        $s = Cache::remember('absensi_s_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'S')->get();
+        });
+        $sd = Cache::remember('absensi_sd_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'SD')->get();
+        });
+        $iz = Cache::remember('absensi_iz_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'I')->get();
+        });
+        $itd = Cache::remember('absensi_itd_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'ITD')->get();
+        });
+        $icp = Cache::remember('absensi_icp_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'ICP')->get();
+        });
+        $td = Cache::remember('absensi_td_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'TD')->get();
+        });
+        $cp = Cache::remember('absensi_cp_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Absensi::where('nik', $user->nik)->where('jenis', 'CP')->get();
+        });
+        $ochi = Cache::remember('ochi_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Ochi::where('nik', $user->nik)->get();
+        });
+    
+        $qcc = Cache::remember('qcc_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Qcc::where('nik', $user->nik)->get();
+        });
+        $oleader = Cache::remember('oleader_' . $user->nik, Carbon::now()->addMinutes(10), function () use ($user) {
+            return Ochi::where('nik_ochi_leader', $user->nik)->get();
+        });
+        // $a = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'A')
+        //     ->get();
+        // $s = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'S')
+        //     ->get();
+        // $sd = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'SD')
+        //     ->get();
+        // $iz = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'I')
+        //     ->get();
+        // $itd = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'ITD')
+        //     ->get();
+        // $icp = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'ICP')
+        //     ->get();
+        // $td = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'TD')
+        //     ->get();
+        // $cp = Absensi::where('nik', $user->nik)
+        //     ->where('jenis', 'CP')
+        //     ->get();
+        // $ochi = Ochi::where('nik', $user->nik)->get();
+        // $qcc = Qcc::where('nik', $user->nik)->get();
+        // $oleader = Ochi::where('nik_ochi_leader', $user->nik)->get();
         // $jochi = Ochi::where('nik', $user->nik)
         //     ->whereIn('juara', ['Juara 1', 'Juara 2', 'Juara 3'])
         //     ->get();
