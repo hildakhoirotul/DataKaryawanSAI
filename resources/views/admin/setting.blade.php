a9de9700e2ff968fc5b5de48f5fcc6e576908398
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-08-30 10:12:58 +0700

    revisi livewire

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -36,6 +36,6 @@
         </div>
     </div>
 </main>
-@livewireScripts
+<!-- @livewireScripts -->
 <script src="{{ asset('/vendor/livewire/livewire.js?id=90730a3b0e7144480175') }}" data-turbo-eval="false" data-turbolinks-eval="false" ></script>
 @endsection
